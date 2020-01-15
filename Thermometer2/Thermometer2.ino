ab1aa4ed2f605d2a6643ff5d9a723d92a8ccc369
kevinzluo <44309027+kevinzluo@users.noreply.github.com>
2020-01-15 16:18:15 -0600

    log files and new coefficients

--- Thermometer2/Thermometer2.ino
+++ Thermometer2/Thermometer2.ino
@@ -1,13 +1,11 @@
 int tempPin = 0;
-//                BS E  D4 D5  D6  D7
-//LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 int index = 0;
 double breakTemp1 = 27.00;
 double breakTemp2 = 30.00;
 
-int state = 0;
+//int state = 0;
 //                R  G  B
-int ledPorts[] = {3, 2, 4};
+int ledPorts[] = {2, 3, 4};
 
 
 int LED_RED = ledPorts[0];
@@ -20,7 +18,7 @@ void setup()
   Serial.begin(9600);
 
   for (int pin : ledPorts) {
-    //pinMode(pin, OUTPUT);
+    pinMode(pin, OUTPUT);
   }
 }
 
@@ -48,22 +46,23 @@ void loop()
   // Steinhart-Hart: 1/T = A + B ln R + C (ln R)^3
   //                 T = 1 / (A + (B + (C (ln R)^2)) * ln R)
   double lnR = log(resistance);
-  double tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * lnR * lnR )) * lnR ); // Steinhart-Hart
-
-  float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
-  float tempF = (tempC * 9.0) / 5.0 + 32.0; // Convert Celcius to Fahrenheit
+  double tempK1 = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * lnR * lnR )) * lnR ); // Steinhart-Hart
+  double tempK2 = 1 / ( 0.001167419 + (0.000226582) * lnR + 0.000000142456334686082 * lnR * lnR * lnR );
+  float tempC1 = tempK1 - 273.15;            // Convert Kelvin to Celcius
+  float tempC2 = tempK2 - 273.15;
+  //float tempF = (tempC * 9.0) / 5.0 + 32.0; // Convert Celcius to Fahrenheit
 
   // Display Temperature in C
   Serial.print(String(index) + "\t(analog): " + String(tempReading) + "\t");
-  Serial.println(String(index) + "(tempC): " + String(tempC));
+  Serial.println(String(index) + "(tempC1): " + String(tempC1) + "\t" + "(tempC2):" + String(tempC2));
 
   
-  digitalWrite(LED_RED, indRed(tempC));
-  digitalWrite(LED_GREEN, indGreen(tempC));
-  digitalWrite(LED_BLUE, indBlue(tempC));
+  digitalWrite(LED_RED, indRed(tempC1));
+  digitalWrite(LED_GREEN, indGreen(tempC1));
+  digitalWrite(LED_BLUE, indBlue(tempC1));
   
 
-  Serial.println("\tRed: " + String(indRed(tempC)) + "\tGreen: " + String(indGreen(tempC)) + "\tBlue: " + String(indBlue(tempC)));
+  Serial.println("\tRed: " + String(indRed(tempC1)) + "\tGreen: " + String(indGreen(tempC1)) + "\tBlue: " + String(indBlue(tempC1)));
   /*
    * red < breakTemp1 <= green < breakTemp2 <= blue
     
